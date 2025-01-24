bd2ff3470544d2fd973717ee62fcfd6b1d481b70
yenminh269 <hhyenminh2001@gmail.com>
2025-01-23 22:53:05 -0600

    add DOM events and methods to display the grid as user request

--- java.js
+++ java.js
@@ -1,29 +1,37 @@
-let container = document.querySelector("#container");
-let row = 16;
-let column = 16;
-for(let i =0; i<row;i++){
-    for(let j=0; j<column;j++){
-        let div = document.createElement("div");
-        div.style.width = "50px";
-        div.style.height = "50px";
-        div.style.border = "solid 2px grey";
-        div.style.color= "blue";
+let user =0;
+let button1 = document.querySelector("#button1");
+button1.addEventListener('click',() =>{
+    user = prompt("Type the number of squares per side: ");
+    document.body.removeChild(container);
+    container = document.createElement("div");
+    container.classList.add("container");
+    playSketch(user);
+})
+let container = document.querySelector(".container");
+
+playSketch(16);
+function playSketch(squares){
+    container.style.width = "500px";
+    container.style.height = "500px";
+    let size = 500/squares;
+    for(let i =0; i<squares*squares;i++){
+            let div = document.createElement("div");
+            div.style.width = div.style.height = `${size-4}px`;
+            div.style.border = "solid 2px grey";
+            div.style.color= "black";
+
+            div.addEventListener('mouseover', ()=>{
+                div.classList.add("hovered");
+            });
+            div.addEventListener('mouseout', ()=>{
+                div.classList.remove("hovered");
+                div.classList.add("unhovered");
+            });
+            div.innerHTML = `${i+1}`;
+            container.appendChild(div);
         
-        div.classList.add("new");
-        div.innerHTML = `${j+1}`;
-        container.appendChild(div);
     }
+    document.body.appendChild(container);
 }
-document.body.appendChild(container);
-let elements = document.querySelectorAll(".new");
-for(element of elements){
 
 
-    element.addEventListener('mouseover', ()=>{
-        element.classList.add("hovered");
-    })
-    element.addEventListener('mouseout', ()=>{
-        element.classList.remove("hovered");
-        element.classList.add("unhovered");
-    })
-}
